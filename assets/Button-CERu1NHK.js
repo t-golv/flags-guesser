9747e6eb348392d719b8851c22bdfef825ab4442
Th <114317304+t-golv@users.noreply.github.com>
2025-09-27 18:36:34 -0300

    Updates

---
+++ assets/Button-CERu1NHK.js
@@ -0,0 +1,3 @@
+import{t as i,l,i as s,d as n,x as o,e as c}from"./index-q3Z7grX_.js";var v=i("<button>");const b=a=>(()=>{var e=v();return l(e,"click",a.onClick,!0),s(e,()=>a.children),n(t=>{var r=a.disabled,d=`px-10 py-4 text-xl font-semibold rounded-4xl transition cursor-pointer
+        bg-gray-700 text-white hover:bg-gray-600 disabled:opacity-50 disabled:cursor-not-allowed
+        ${a.class??""}`;return r!==t.e&&(e.disabled=t.e=r),d!==t.t&&o(e,t.t=d),t},{e:void 0,t:void 0}),e})();c(["click"]);export{b as B};
